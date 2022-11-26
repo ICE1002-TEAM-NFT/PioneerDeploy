fa6f0cd4399293a15cfdd9bd5d33bc0f1637ac2a
이창용 <104843976+era-s@users.noreply.github.com>
2022-11-26 14:12:31 +0900

    gunicorn fixed

--- flask/api/app.py
+++ flask/api/app.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, redirect, render_template, request, session, url_for
-
+from flask_mqtt import Mqtt
 
 def create_app():
+
     app = Flask(__name__)
-    
+
     @app.route('/')
     def home():
         return render_template('index.html')
     
-    @app.route('/2')
-    def home():
+    @app.route('/1')
+    def home2():
         return render_template('index2.html')
     
     return app
